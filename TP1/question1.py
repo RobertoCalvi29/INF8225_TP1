still need to finish ADAM and numerical instability to be fix

TP1/question1.py
@@ -1,5 +1,4 @@
 import numpy as np
-# _________________________________________________Question 1___________________________________________________________
 
 prob_pluie = np.array([0.8, 0.2]).reshape(2, 1, 1, 1)
 print("Pr(Pluie) = {}\n".format(np.squeeze(prob_pluie)))
@@ -20,6 +19,8 @@ print("Pr(Holmes|Pluie,Arroseur)={}\n".format(np.squeeze(holmes)))
 watson[0, :, 1, :]  # prob watson mouille−pluie
 watson_mouille = (watson * prob_pluie).sum(0).squeeze()[1]  # prob gazon watson mouille
 
+table_prob = holmes * watson * prob_pluie * prob_arroseur
+
 # a)____________________________________________________________________________________________________________________
 # Ce qui revient à
 #
@@ -33,39 +34,29 @@ holmes_mouille = ((holmes * prob_pluie).sum(0) * prob_arroseur).sum(1).squeeze()
 print("Pr(Holmes=1|Pluie,arroseur)={}\n".format(np.squeeze(holmes_mouille)))
 
 # b)____________________________________________________________________________________________________________________
-# En réutilisant simplement le résultat obtenue en a) et en se servant des données fournis
-# on a Pr(H=1|W=1) = Pr(Holmes=1|Pluie,arroseur) *  Pr(Watson=1|Pluie).
-
-holmes1_watson1 = holmes_mouille * watson_mouille
+holmes1_watson1 = table_prob.sum(0).sum(0).squeeze()[1][1] / watson_mouille
 print("Pr(Holmes=1|Watson=1)={}\n".format(np.squeeze(holmes1_watson1)))
 
 # c)__________________________________________________________________________________________________________
-# Toujours en réutilisant en réutilisant simplement le résultat obtenue en a) et en se servant des données fournis
-# on a Pr(H=1|W=0) = Pr(Holmes=1|Pluie,arroseur) *  Pr(Watson=0|Pluie).
-
-watson_sec = (watson * prob_pluie).sum(0).squeeze()[0]  # prob gazon watson mouille
-holmes1_watson0 = holmes_mouille * watson_sec
+watson_sec = (watson * prob_pluie).sum(0).squeeze()[0]
+holmes1_watson0 = table_prob.sum(0).sum(0).squeeze()[1][0] / watson_sec
 print("Pr(Holmes=1|Watson=0)={}\n".format(np.squeeze(holmes1_watson0)))
 
 # d)____________________________________________________________________________________________________________________
-# On cherche Pr(H=1|P=0,W=1)
-# Pr(H=1|P=0,W=1) = Pr(Holmes=1|P,A) *  Pr(Pluie=0)
-#  on a  alors Pr(H=1|P=0,W=1) = Pr(Holmes=1|Watson =1) *  Pr(Pluie=0).
-
-holmes1_P0_watson_1 = holmes1_watson1 * prob_pluie[0]
+holmes1_P0_watson_1 = (holmes * prob_arroseur).sum(1).squeeze()[0][0]
 print("Pr(Holmes=1|P=0,Watson=1)={}\n".format(np.squeeze(holmes1_P0_watson_1)))
 
 # e)____________________________________________________________________________________________________________________
 # On cherche Pr(W=1|H=1)
 # On applique simplement la règle de Bayes
 #  Pr(W=1|H=1) = (Pr(H=1|W=1)*P(W=1))/P(H=1)
-
 watson1_holmes1 = (holmes1_watson1 * watson_mouille) / holmes_mouille
 print("Pr(Watson=1|Holmes=1)={}\n".format(np.squeeze(watson1_holmes1)))
 
 # f)____________________________________________________________________________________________________________________
 # On cherche Pr(W=1|H=1,A=1)
-#  Pr(W=1|H=1,A=1) = Pr(W=1|H=1)*P(A=1) ... pas sur
+#  Pr(W=1|H=1,A=1) = Pr(W=1|H=1)*P(A=1)
 
-watson1_holmes1_A1 = watson1_holmes1 * 0.1
+watson1_holmes1_A1 = (watson * holmes * prob_pluie * prob_arroseur).sum(0).squeeze() / \
+                     (watson * holmes * prob_pluie * prob_arroseur).sum(2).sum(0)
 print("Pr(Watson=1|Holmes=1)={}\n".format(np.squeeze(watson1_holmes1_A1)))